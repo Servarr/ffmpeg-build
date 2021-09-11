8ed37ec9fa4e334f7131494874f3eb640ec1dc37
ta264 <ta264@users.noreply.github.com>
2021-09-11 13:43:40 +0100

    Add linux-musl-x64

---
+++ linux-musl-x64/compile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pushd $SCRIPT_DIR
+
+./configure \
+    --prefix="/ffmpeg/build" \
+    --bindir="/ffmpeg/bin" \
+    --disable-ffplay \
+    --disable-ffmpeg \
+    --disable-encoders \
+    --disable-filters \
+    --disable-devices \
+    --disable-muxers \
+    --disable-protocols \
+    --enable-protocol=file \
+    --disable-bsfs \
+    --disable-network \
+    --disable-doc \
+    --pkg-config-flags="--static" \
+    --extra-ldexeflags="-static" \
+    --extra-libs="-lpthread -lm"
+
+make -j6
+make install
+
+popd
