a85f41977288446b8518a5a6a2900b6ffabc716e
ta264 <ta264@users.noreply.github.com>
2022-04-12 19:46:43 +0100

    Add linux-x86 support

---
+++ linux-x86/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SOURCE_DIR="${SCRIPT_DIR}/../ffmpeg"
+OUT_DIR="${SCRIPT_DIR}/output"
+
+mkdir -p $OUT_DIR
+
+docker build -t ffmpeg-linux-x86 ${SCRIPT_DIR} < ${SCRIPT_DIR}/Dockerfile
+
+cp "${SCRIPT_DIR}/../common.sh" "${SOURCE_DIR}"
+cp "${SCRIPT_DIR}/compile.sh" "${SOURCE_DIR}"
+
+docker run --rm --rm -v "${SOURCE_DIR}:/ffmpeg/sources" -v "${OUT_DIR}:/ffmpeg/bin" ffmpeg-linux-x86 /ffmpeg/sources/compile.sh
+
+rm "${SOURCE_DIR}/compile.sh"
+rm "${SOURCE_DIR}/common.sh"
---
+++ linux-x86/compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pushd $SCRIPT_DIR
+
+source common.sh
+
+FFMPEG_CONFIGURE_FLAGS+=(
+    --enable-cross-compile
+    --arch=x86
+    --target-os=linux
+    --cross-prefix=i686-linux-gnu-
+    --pkg-config=pkg-config
+    --pkg-config-flags="--static"
+    --extra-ldexeflags="-static"
+    --extra-libs="-lpthread -lm"
+)
+
+ARGS=("${DOCKER_DIR_FLAGS[@]}" "${FFMPEG_CONFIGURE_FLAGS[@]}")
+PKG_CONFIG_PATH=/usr/i686-linux-gnu/lib/pkgconfig ./configure "${ARGS[@]}"
+
+make -j6
+make install
+
+popd
