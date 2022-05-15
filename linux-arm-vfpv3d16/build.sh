59780a113d756fc77312963469c0c2b1aa71f00d
ta264 <ta264@users.noreply.github.com>
2022-05-15 21:31:42 +0100

    Add linux-arm-vfpv3d16 support

---
+++ linux-arm-vfpv3d16/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SOURCE_DIR="${SCRIPT_DIR}/../ffmpeg"
+OUT_DIR="${SCRIPT_DIR}/output"
+
+mkdir -p $OUT_DIR
+
+docker build -t ffmpeg-linux-arm-vfpv3d16 ${SCRIPT_DIR} < ${SCRIPT_DIR}/Dockerfile
+
+cp "${SCRIPT_DIR}/../common.sh" "${SOURCE_DIR}"
+cp "${SCRIPT_DIR}/compile.sh" "${SOURCE_DIR}"
+
+docker run --rm -v "${SOURCE_DIR}:/ffmpeg/sources" -v "${OUT_DIR}:/ffmpeg/bin" ffmpeg-linux-arm-vfpv3d16 /ffmpeg/sources/compile.sh
+
+rm "${SOURCE_DIR}/compile.sh"
+rm "${SOURCE_DIR}/common.sh"
