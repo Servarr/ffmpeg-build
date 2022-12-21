db161a70e8785031fc1a8e21359a1c42b241d692
Martin Häger <martin.haeger@gmail.com>
2022-12-21 21:32:20 +0100

    macOS Intel: rebuild libbluray without fontconfig and freetype

--- osx-x64/build.sh
+++ osx-x64/build.sh
@@ -5,9 +5,13 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SOURCE_DIR="${SCRIPT_DIR}/../ffmpeg"
 
-brew install automake shtool nasm libbluray dav1d dylibbundler
+brew install automake shtool nasm dav1d dylibbundler
 
 mkdir -p "${SCRIPT_DIR}/output" "${SCRIPT_DIR}/build"
+
+brew cat libbluray > "${SCRIPT_DIR}/build/libbluray.rb"
+sed -i '' -e 's/%W\[/&--without-fontconfig --without-freetype /; s/depends_on "fontconfig"/#&/; s/depends_on "freetype"/#&/' "${SCRIPT_DIR}/build/libbluray.rb"
+brew reinstall --build-from-source "${SCRIPT_DIR}/build/libbluray.rb"
 
 cp "${SCRIPT_DIR}/../common.sh" "${SOURCE_DIR}"
 cp "${SCRIPT_DIR}/compile.sh" "${SOURCE_DIR}"
