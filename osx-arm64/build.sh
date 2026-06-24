29206dd2e00b3e159aa618b1917495e9a6948a35
RobinDadswell <19610103+RobinDadswell@users.noreply.github.com>
2026-06-24 19:08:49 +0100

    5.1.10

--- osx-arm64/build.sh
+++ osx-arm64/build.sh
@@ -5,19 +5,23 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SOURCE_DIR="${SCRIPT_DIR}/../ffmpeg"
 
-brew install automake shtool nasm dylibbundler
-
-# The arm64 version of dav1d to link against
-response=$(brew fetch --force --bottle-tag=arm64_big_sur dav1d | grep "Downloaded to")
-parsed=${response#Downloaded to: }
-brew install -v "$parsed"
+brew install meson nasm dylibbundler
 
 mkdir -p "${SCRIPT_DIR}/output" "${SCRIPT_DIR}/build"
 
+rm -rf "${SCRIPT_DIR}/build/dav1d"
+git clone --depth 1 --branch 1.0.0 https://code.videolan.org/videolan/dav1d.git "${SCRIPT_DIR}/build/dav1d" && \
+mkdir -p "${SCRIPT_DIR}/build/dav1d/build" && \
+cd "${SCRIPT_DIR}/build/dav1d/build" && \
+meson setup -Denable_tools=false -Denable_tests=false --default-library=static --prefix="${SCRIPT_DIR}/build/dav1d-prefix" --libdir=lib --cross-file="${SCRIPT_DIR}/aarch64-apple-darwin.meson" .. && \
+ninja && \
+ninja install
+
 cp "${SCRIPT_DIR}/../common.sh" "${SOURCE_DIR}"
 cp "${SCRIPT_DIR}/compile.sh" "${SOURCE_DIR}"
+export PKG_CONFIG_PATH="${SCRIPT_DIR}/build/dav1d-prefix/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 
-${SOURCE_DIR}/compile.sh $SCRIPT_DIR
+"${SOURCE_DIR}/compile.sh" "${SCRIPT_DIR}"
 
 rm "${SOURCE_DIR}/compile.sh"
 rm "${SOURCE_DIR}/common.sh"
